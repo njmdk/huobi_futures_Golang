af936bf89e3485c217146e0cc5b61044ea247254
“VincentCai” <vincent.cai@htx-inc.com>
2024-06-21 16:08:58 +0800

    /linear-swap-api/v3/fix_position_margin_change add response param amount, asset, contract_code, type and direction

--- sdk/linearswap/restful/response/unifiedaccount/fix_position_margin_change_response.go
+++ sdk/linearswap/restful/response/unifiedaccount/fix_position_margin_change_response.go
@@ -5,7 +5,12 @@ type FixPositionMarginChangeResponse struct {
 	Msg  string `json:"msg"`
 	Ts   int64  `json:"ts"`
 	Data struct {
-		OrderID       string `json:"order_id"`
-		ClientOrderID int64  `json:"client_order_id"`
+		OrderID       string  `json:"order_id"`
+		ClientOrderID int64   `json:"client_order_id"`
+		Amount        float64 `json:"amount"`
+		Asset         string  `json:"asset"`
+		ContractCode  string  `json:"contract_code"`
+		Type          int     `json:"type"`
+		Direction     int     `json:"direction"`
 	} `json:"data"`
 }
